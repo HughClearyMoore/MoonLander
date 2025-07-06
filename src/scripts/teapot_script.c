3cd39f15ab840a204cd4ab932c193c214a2862c4
Hugh Cleary-Moore <hughclearymoore@outlook.com>
2025-07-06 16:27:35 +1000

    Made cubes spawn with left shift

--- src/scripts/teapot_script.c
+++ src/scripts/teapot_script.c
@@ -4,8 +4,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#include <cglm/cglm.h>
 
 #include "MLCore.h"
+
+static double MakeRandom()
+{
+	return rand() / (double)RAND_MAX;
+}
 
 DEFAULT_CREATE_FUNCTION(TeapotScript, TeapotCtx)
 
@@ -19,6 +27,7 @@ READY_FUNCTION(TeapotScript)
 
 UPDATE_FUNCTION(TeapotScript)
 {
+	Input* input = GET_INPUT;
 	TeapotCtx* teapot = (TeapotCtx*)ctx;
 
 	Transform* t = MLECSGetComponentTransform(&game_ctx->ecs, entity);
@@ -29,6 +38,39 @@ UPDATE_FUNCTION(TeapotScript)
 	if (t->scale == 0.0)
 	{
 		MLECSDestroyEntity(&game_ctx->ecs, entity);
+	}
+
+	if (input->keys[GLFW_KEY_LEFT_SHIFT].just_pressed)
+	{
+		Entity_t new_entity = MLECSNewEntity(&game_ctx->ecs);
+
+		
+		vec3 direction = { MakeRandom(), MakeRandom(), MakeRandom() };
+		glm_normalize(direction);
+
+		double distance = 1 + MakeRandom() * 10.0;
+
+		glm_vec3_scale(direction, (float)distance, direction);
+
+		vec3 final_pos = { 0.0, 0.0, 0 };
+		glm_vec3_add(final_pos, direction, final_pos);
+
+		Transform trans =
+		{
+			.x = final_pos[0],
+			.y = final_pos[1],
+			.z = final_pos[2],
+			.scale = MakeRandom() * 3 + 1.0
+		};
+
+		MLECSAttachComponentTransform(&game_ctx->ecs, new_entity, &trans);
+
+		Mesh* mesh = MLMeshManagerGetMesh(&game_ctx->mesh_manager, "cube");
+		ShaderProgram* program = MLShaderProgramManagerGetProgram(&game_ctx->program_manager, "basic");
+
+		Model model = { .mesh = mesh, .program = program };
+
+		MLECSAttachComponentModel(&game_ctx->ecs, new_entity, &model);
 	}
 }
 
